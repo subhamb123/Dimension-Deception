6e90c2e72c5fe5cc288b080ac3e5befe188adeb1
Vishal Devireddy <TwoTau@users.noreply.github.com>
2019-03-02 15:07:05 -0800

    Add socket.io on client

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const server = require("http").createServer(app);
-// const io = require("../..")(server);
+var io = require("socket.io")(server);
 const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
@@ -11,4 +11,8 @@ server.listen(port, () => {
 });
 
 // Routing
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "client")));
+
+io.on('connection', function(socket){
+	console.log('a user connected');
+});
