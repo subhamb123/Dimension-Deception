98e677e613a0a99ffb7cc8c304803d8f76cf27a8
Vishal Devireddy <TwoTau@users.noreply.github.com>
2019-03-02 23:08:12 -0800

    Add trees and boundary box

--- client/client.js
+++ client/client.js
@@ -1,10 +1,13 @@
 const socket = io();
 
+const BACKGROUND_COLOR = 0x282C34;
+
 let app = new PIXI.Application({
 	width: 500,
 	height: 500,
 	antialias: true,
 	transparent: false,
+	backgroundColor: BACKGROUND_COLOR,
 	resolution: 1
 });
 app.renderer.view.style.position = "absolute";
@@ -22,6 +25,8 @@ const TILE_SIZE = 50;
 let userTileX = 10;
 let userTileY = 10;
 let userLevel = 0;
+let levelMaxTileX = 1000;
+let levelMaxTileY = 1000;
 
 let userSpeed = 10;
 const BULLET_SPEED = 20;
@@ -54,15 +59,15 @@ let controls = {
 
 const horizontalLines = [];
 const verticalLines = [];
+drawLines();
+const boundaryBox = createBoundaryBox();
 
 let gameState = {
 	players: [],
 	bullets: [],
-	trees: [],
+	trees: createRandomTrees(5),
 	rocks: []
 };
-
-drawLines();
 
 const userSprite = createUserSprite();
 
@@ -99,13 +104,13 @@ function makeBullet(bullet) {
 	gameState.bullets.push(circle);
 }
 
-setup();
-
 function createUserSprite() {
 	const circle = new PIXI.Graphics();
 	const OUTLINE_WIDTH = 10;
 	circle.lineStyle(OUTLINE_WIDTH, 0x6C3ACC, 1);
+	circle.beginFill(BACKGROUND_COLOR);
 	circle.drawCircle(0, 0, (TILE_SIZE - OUTLINE_WIDTH) / 2);
+	circle.endFill();
 	circle.x = app.renderer.width / 2;
 	circle.y = app.renderer.height / 2;
 
@@ -113,43 +118,96 @@ function createUserSprite() {
 	return circle;
 }
 
+function createBoundaryBox() {
+	const rect = new PIXI.Graphics();
+	rect.lineStyle(6, 0xCCCCCC, 1);
+	rect.drawRect(-TILE_SIZE / 2,- TILE_SIZE / 2, levelMaxTileX + TILE_SIZE, levelMaxTileY + TILE_SIZE);
+	stage.addChild(rect);
+	return rect;
+}
+
+function createRandomTrees(n) {
+	const trees = [];
+	for (let i = 0; i < n; i++) {
+		const circle = new PIXI.Graphics();
+		const OUTLINE_WIDTH = 20;
+		const RADIUS = (Math.random() + 0.5) * TILE_SIZE;
+		circle.lineStyle(OUTLINE_WIDTH, 0x17B530, 1);
+		circle.beginFill(0x21D53D);
+		circle.drawCircle(0, 0, RADIUS);
+		circle.endFill();
+		circle.tileX = levelMaxTileX * Math.random();
+		circle.tileY = levelMaxTileY * Math.random();
+		circle.radius = RADIUS;
+
+		stage.addChild(circle);
+		trees.push(circle);
+	}
+	return trees;
+}
+
 // draws a grid of horizontal and vertical lines
 function drawLines() {
+	const LINE_COLOR = 0x3C4252;
+	const X_OFFSET = (app.renderer.width / 2) % TILE_SIZE;
 	for (let i = 0; i < app.renderer.width + TILE_SIZE; i += TILE_SIZE) {
 		const line = new PIXI.Graphics();
-		line.lineStyle(1, 0x333333, 1);
-		line.moveTo(i, 0);
-		line.lineTo(i, app.renderer.height);
+		line.lineStyle(1, LINE_COLOR, 1);
+		line.moveTo(i + X_OFFSET, 0);
+		line.lineTo(i + X_OFFSET, app.renderer.height);
 		verticalLines.push(line);
 		app.stage.addChild(line);
 	}
+	const Y_OFFSET = (app.renderer.height / 2) % TILE_SIZE;
 	for (let j = 0; j < app.renderer.height + TILE_SIZE; j += TILE_SIZE) {
 		const line = new PIXI.Graphics();
-		line.lineStyle(1, 0x333333, 1);
-		line.moveTo(0, j);
-		line.lineTo(app.renderer.width, j);
+		line.lineStyle(1, LINE_COLOR, 1);
+		line.moveTo(0, j + Y_OFFSET);
+		line.lineTo(app.renderer.width, j + Y_OFFSET);
 		horizontalLines.push(line);
 		app.stage.addChild(line);
 	}
 }
 
 function setup() {
-
 	app.renderer.render(app.stage);
 	app.ticker.add(delta => gameLoop(delta));
 }
 
+function isOutOfBounds(x, y) {
+	return (x < 0 || y < 0 || x > levelMaxTileX || y > levelMaxTileY);
+}
+
+function isValidPos(x, y, userRadius) {
+	for (let tree of gameState.trees) {
+		const distance = Math.hypot(x - tree.tileX, y - tree.tileY);
+		if (distance < userRadius + tree.radius) {
+			return false;
+		}
+	}
+	return true;
+}
+
 function gameLoop(delta) {
-    let intendedX = userTileX;
-    let intendedY = userTileY;
-	if (controls.up.isDown || controls.w.isDown) {intendedY -= userSpeed/delta;}
-	if (controls.left.isDown || controls.a.isDown) {intendedX -= userSpeed/delta;}
-	if (controls.down.isDown || controls.s.isDown) {intendedY += userSpeed/delta;}
-    if (controls.right.isDown || controls.d.isDown) {intendedX += userSpeed/delta;}
-    
-    //Collsion detection
-    userTileX = intendedX;
-    userTileY = intendedY;
+	const newPos = {
+		x: userTileX,
+		y: userTileY
+	};
+
+	if (controls.up.isDown || controls.w.isDown) {newPos.y -= userSpeed / delta;}
+	if (controls.left.isDown || controls.a.isDown) {newPos.x -= userSpeed / delta;}
+	if (controls.down.isDown || controls.s.isDown) {newPos.y += userSpeed / delta;}
+	if (controls.right.isDown || controls.d.isDown) {newPos.x += userSpeed / delta;}
+
+	if (newPos.x < 0) {newPos.x = 0;}
+	if (newPos.y < 0) {newPos.y = 0;}
+	if (newPos.x > levelMaxTileX) {newPos.x = levelMaxTileX;}
+	if (newPos.y > levelMaxTileY) {newPos.y = levelMaxTileY;}
+
+	if (isValidPos(newPos.x, newPos.y, TILE_SIZE / 2)) {
+		userTileX = newPos.x;
+		userTileY = newPos.y;
+	}
 
 	for (let line of verticalLines) {
 		line.position.x = -userTileX % TILE_SIZE;
@@ -158,12 +216,32 @@ function gameLoop(delta) {
 		line.position.y = -userTileY % TILE_SIZE;
 	}
 
-	for (let bullet of gameState.bullets) {
+	boundaryBox.x = app.renderer.width / 2 - userTileX;
+	boundaryBox.y = app.renderer.height / 2 - userTileY;
+
+	for (let i = gameState.bullets.length - 1; i >= 0; i--) {
+		const bullet = gameState.bullets[i];
 		bullet.tileX += bullet.vx / delta;
 		bullet.tileY += bullet.vy / delta;
-		bullet.x = (bullet.tileX - userTileX) + app.renderer.width / 2;
-		bullet.y = (bullet.tileY - userTileY) + app.renderer.height / 2;
+		if (isOutOfBounds(bullet.tileX, bullet.tileY)) {
+			gameState.bullets.splice(i, 1);
+			app.stage.removeChild(bullet);
+		} else {
+			bullet.x = (bullet.tileX - userTileX) + app.renderer.width / 2;
+			bullet.y = (bullet.tileY - userTileY) + app.renderer.height / 2;
+		}
 	}
 
-	DATA_ELEMENT.innerHTML = `x: ${Math.floor(userTileX * 100)/100}, y: ${Math.floor(userTileY * 100)/100}`;
+	for (let tree of gameState.trees) {
+		tree.x = (tree.tileX - userTileX) + app.renderer.width / 2;
+		tree.y = (tree.tileY - userTileY) + app.renderer.height / 2;
+	}
+	for (let rock of gameState.rocks) {
+		rock.x = (rock.tileX - userTileX) + app.renderer.width / 2;
+		rock.y = (rock.tileY - userTileY) + app.renderer.height / 2;
+	}
+
+	DATA_ELEMENT.innerHTML = `x: ${Math.round(userTileX)}, y: ${Math.round(userTileY)}`;
 }
+
+setup();
